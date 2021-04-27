7be6fd2d489da1bbe4031e9e9bd90a300aecb4ed
Paul St. Amour <paul35444@aol.com>
2021-04-27 11:46:51 -0500

    -print state for content_length

--- CodeInjector.py
+++ CodeInjector.py
@@ -32,7 +32,7 @@ def process_packet(packet):
             if content_length_search:
 #group(1) matches second item from entire str above (\d*) digits
                 content_length = content_length_search.group(1)
-                print(content_length)
+                
 #if state if load is not the same as the scapy packet Raw layer load then modify the packet
         if load != scapy_packet[scapy.Raw].load:
 #create new packet by replacing captured scapy_packet with the new modified load
